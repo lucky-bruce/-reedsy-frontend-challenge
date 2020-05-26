0ef303f590b7bab1950de511b7ad623c324e3894
lucky-bruce <jalicorich@gmail.com>
2020-05-26 07:15:52 +0300

    feat: add pagination

--- q4/src/app/books/books.component.ts
+++ q4/src/app/books/books.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Book } from '../core/models/book';
-import { PageSizeDefault, PageSizeSmall } from '../core/models/paginator';
+import { PageSizeSmall } from '../core/models/paginator';
 import { BooksService } from '../core/services/books.service';
 import { ToastrService } from '../core/services/toastr.service';
 import { NgxSpinnerService } from 'ngx-spinner';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'reedsy-books',
@@ -15,27 +15,33 @@ import { ActivatedRoute } from '@angular/router';
 export class BooksComponent implements OnInit {
 
   books: Book[] = [];
-  page = this.route.snapshot.queryParams.page || 0;
+  page = this.route.snapshot.queryParams.page || 1;
   total = 0;
   take = PageSizeSmall;
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private spinner: NgxSpinnerService,
     private booksService: BooksService,
     private toastrService: ToastrService,
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
     this.loadBooks();
   }
 
-
+  pageChange(page: number) {
+    this.router.navigate(['/books'], { queryParams: { page } });
+    this.page = page;
+    this.loadBooks();
+  }
 
   async loadBooks() {
     try {
       this.spinner.show();
-      const res = await this.booksService.books(this.page * this.take, this.take).toPromise();
+      const res = await this.booksService.books((this.page - 1) * this.take, this.take).toPromise();
       this.books = res.data;
       this.total = res.count;
     } catch (e) {
